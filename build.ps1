e5dba5c01baeadf5811193fa2808b9c4e41c882d
smacken <scott.mac.web@gmail.com>
2013-01-08 09:46:56 -0800

    psake build start

---
+++ build.ps1
@@ -0,0 +1,46 @@
+﻿# build functions to assist with the build process
+
+# Configure the formatting of the tasks within the build console log
+formatTaskName {
+	param($taskName)
+	write-host $taskName -foregroundcolor Green
+}
+
+function Write-Today()
+{
+	get-date
+}
+
+function Write-Documentation ()
+{
+	"Each task can be run by using the command: psake"
+}
+
+function Create-Folder($folderName)
+{
+	new-item $folderName -itemtype Directory | out-null
+}
+
+function Delete-Directory($folderName)
+{
+	remove-item -force -recurse $folderName -erroraction SilentlyContinue
+}
+
+function New-BalloonTip {
+[CmdletBinding()]
+Param(
+$TipText='This is the body text.',
+$TipTitle='This is the title.',
+$TipDuration='10000'
+)
+	[system.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms') | Out-Null
+	$balloon = New-Object System.Windows.Forms.NotifyIcon
+	$path = Get-Process -id $pid | Select-Object -ExpandProperty Path
+	$icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
+	$balloon.Icon = $icon
+	$balloon.BalloonTipIcon = 'Info'
+	$balloon.BalloonTipText = $TipText
+	$balloon.BalloonTipTitle = $TipTitle
+	$balloon.Visible = $true
+	$balloon.ShowBalloonTip($TipDuration)
+}
---
+++ default.ps1
@@ -0,0 +1,68 @@
+﻿include ".\build.ps1"
+
+Framework "4.0"
+
+properties {
+	$applicationName = "Migrationist"
+	$buildConfig = "Debug"
+}
+
+properties {
+  $testMessage = 'Executed Test!'
+  $compileMessage = 'Executed Compile!'
+  $cleanMessage = 'Executed Clean!'
+}
+
+properties { # Files
+	$executingDir = new-object System.IO.DirectoryInfo $pwd
+	$baseDir = resolve-path .
+	$sourceDir = "."
+	$solutionFile = "$applicationName.sln"
+	$buildOutputDir = "..\Deploy"
+}
+
+# default task - called with 'psake' command
+task default -depends Test
+
+# Run Tests
+task Test -depends Compile, Clean { 
+  $testMessage
+}
+
+# Compile source code
+task Compile -depends Clean { 
+  
+  msbuild /p:Configuration=$buildConfig /p:OutDir=$buildOutputDir /verbosity:minimal /consoleLoggerparameters:ErrorsOnly /nologo /m "$applicationName.sln"
+  new-balloontip -tiptext "Build" -tiptitle "Compiling"
+  $compileMessage
+}
+
+# Compile Source code
+task CompileDebug -depends Clean { 
+  
+  msbuild /p:Configuration="Debug" /p:OutDir=$buildOutputDir /verbosity:minimal /consoleLoggerparameters:ErrorsOnly /nologo /m "$applicationName.sln"
+  
+  $compileMessage
+}
+
+# Clean the project
+task Clean { 
+  $cleanMessage
+  remove-item .\Deploy\*.*
+  Write-today
+}
+
+# Deploy the project
+task Deploy {
+	msbuild /p:Configuration="Release" /p:OutDir=$buildOutputDir /verbosity:minimal /consoleLoggerparameters:ErrorsOnly /nologo /m "$applicationName.sln"
+	"Deploying"
+}
+
+# Documentation
+task ? -Description "Helper to display task info" {
+	Write-Documentation
+	"psake compile - compile source code"
+	"psake compiledebug - compile source code with"
+	"psake clean"
+	"psake deploy"
+}
